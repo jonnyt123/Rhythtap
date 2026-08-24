264b0360684136b03318456c8be69d92ea5d5756
Jonathan Thompson <bottinoaks6@gmail.com>
2026-08-24 18:33:01 -0400

    Enable player account app transform

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { multiplayerTransform } from './scripts/multiplayer-transform';
+import { accountTransform } from './scripts/account-transform';
 
 export default defineConfig({
-  plugins: [multiplayerTransform(), react()],
+  plugins: [multiplayerTransform(), accountTransform(), react()],
   base: '/Rhythtap/',
 });
