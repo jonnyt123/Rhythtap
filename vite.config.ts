50e14db7f72afc398ad418b7c8551612ae831fc7
Jonathan Thompson <bottinoaks6@gmail.com>
2026-08-25 03:42:41 -0400

    Apply metal menu after existing integrations

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 import { songPackTransform } from './scripts/song-pack-transform';
 import { multiplayerTransform } from './scripts/multiplayer-transform';
 import { accountTransform } from './scripts/account-transform';
+import { metalMenuTransform } from './scripts/metal-menu-transform';
 
 export default defineConfig({
-  plugins: [songPackTransform(), multiplayerTransform(), accountTransform(), react()],
+  plugins: [songPackTransform(), multiplayerTransform(), accountTransform(), metalMenuTransform(), react()],
   base: '/Rhythtap/',
 });
