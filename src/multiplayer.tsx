1a167fc9d823dad70572d1106d09e51120801d1d
Jonathan Thompson <bottinoaks6@gmail.com>
2026-08-30 21:19:50 -0400

    Load multiplayer HUD v2 styles

--- src/multiplayer.tsx
+++ src/multiplayer.tsx
@@ -1,5 +1,6 @@
 import './multiplayer.css';
 import './multiplayer-authoritative.css';
+import './multiplayer-hud-v2.css';
 export * from './multiplayer-common';
 export {MultiplayerLobby} from './multiplayer-lobby';
 export {useMultiplayerSession} from './multiplayer-session';
